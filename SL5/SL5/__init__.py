47c6fe76878e876846c329fa85a42849b00976fa
Mathurin <mathurinpetit@hotmail>
2025-03-27 17:37:39 +0100

    Ajout du SL5

--- SL5/SL5/__init__.py
+++ SL5/SL5/__init__.py
@@ -4,12 +4,11 @@ c = cu
 
 doc = ''
 class C(BaseConstants):
-    NAME_IN_URL = 'VR5'
+    NAME_IN_URL = 'SL5'
     PLAYERS_PER_GROUP = 2
     NUM_ROUNDS = 4
 class Subsession(BaseSubsession):
     pass
-
 def creating_session(subsession: Subsession):
     session = subsession.session
     PROFILES_J2 = {
@@ -33,82 +32,43 @@ def creating_session(subsession: Subsession):
                 # Définition du profil de l'adversaire
         j1.opponent_profile = j2.preference_profile
         j2.opponent_profile = j1.preference_profile
-
-        #Initialisation du score total via participant:
-        for leplayer in [j1, j2]:
-            leplayer.participant.vars["total_score"] = 0
-
+        j1.participant.vars["total_score"] = 0
+        j2.participant.vars["total_score"] = 0
 
 class Group(BaseGroup):
     final_choice = models.StringField()
+
 def calcul_meilleur_choix(group: Group):
-    import random
     player1, player2 = group.get_players()
 
-        # Récupérer les vétos
-    veto_p1 = set(player1.vetos.split(",")) if player1.vetos else set()
-    veto_p2 = set(player2.vetos.split(",")) if player2.vetos else set()
+    # Le choix final est celui sélectionné par J2 (stocké dans rank1)
+    group.final_choice = player2.rank1
 
-        # Exclure les choix véto
-    remaining_choices = set(player1.preference_profile.split(",")) - (veto_p1 | veto_p2)
+            # Attribution des scores en fonction du classement dans `preference_profile`
+    for player in [player1, player2]:
+                if group.final_choice in player.preference_profile.split(","):
+                    player.round_score = 6 - (player.preference_profile.split(",").index(group.final_choice) + 1)
+                else:
+                    player.round_score = 0  # Sécurité si jamais le choix final n'est pas trouvé
 
-        # Récupérer les classements imposés (préférences initiales)
-
-
-        # Récupérer le classement des choix restants
-    ranking_p1 = player1.ranking.split(",")
-    ranking_p2 = player2.ranking.split(",")
-
-        # Attribuer un score à chaque choix restant basé sur `preference_profile`
-    score_choices = {}
-    for choice in remaining_choices:
-            score_p1 = 6 - (ranking_p1.index(choice) + 1) if choice in ranking_p1 else 0
-            score_p2 = 6 - (ranking_p2.index(choice) + 1) if choice in ranking_p2 else 0
-            score_choices[choice] = score_p1 + score_p2  # Somme des scores des deux joueurs
-
-        # Déterminer le(s) choix avec le score maximum
-    max_score = max(score_choices.values(), default=0)
-    best_choices = [c for c, score in score_choices.items() if score == max_score]
-
-        # Tirage au sort en cas d'égalité
-    final_choice = random.choice(best_choices) if best_choices else "Aucun choix possible"
-
-        # Enregistrer le choix final et calculer le score en fonction des préférences initiales
-    for leplayer in [player1, player2]:
-            leplayer.selected_choice = final_choice
-            leplayer.round_score = 6 - (leplayer.preference_profile.split(",").index(final_choice) + 1) if final_choice in leplayer.preference_profile.split(",") else 0
-            #On recupere les valeurs persistantes si elles existent, sinon on les initialise
-
-
-            leplayer.participant.vars["total_score"] += leplayer.round_score
-            #On declare quand meme le total_score local pour qu'il apparaisse dans le csv
-            leplayer.total_score = leplayer.participant.vars["total_score"]
-
-    group.final_choice=final_choice
-
+                # Ajout au score total
+                player.participant.vars["total_score"] += player.round_score
 class Player(BasePlayer):
     preference_profile = models.StringField()
     opponent_profile = models.StringField()
-    vetos = models.StringField()
-    ranking = models.StringField()
     selected_choice = models.StringField()
     round_score = models.IntegerField(initial=0, max=5, min=1)
-    total_score = models.IntegerField(initial=0)
-    veto1 = models.StringField()
-    veto2 = models.StringField()
+    choix = models.StringField()
+    choix1 = models.StringField()
+    choix2 = models.StringField()
+    choix3 = models.StringField()
     rank1 = models.StringField()
-    rank2 = models.StringField()
-    rank3 = models.StringField()
+
 class Page_Accueil(Page):
     form_model = 'player'
     @staticmethod
     def is_displayed(player: Player):
         return player.round_number == 1
-
-    @staticmethod
-    def vars_for_template(player: Player):
-        return {'total_score': player.participant.vars["total_score"]}
-
 class Presentation_VR(Page):
     form_model = 'player'
     @staticmethod
@@ -130,9 +90,14 @@ class Attribution_des_profils(Page):
                     'combined_profiles': combined_profiles,  # Liste [(1, 'A', 'E'), (2, 'B', 'D'), ...]
                     'total_score': player.participant.vars["total_score"]
                 }
-class Veto_Et_Classement(Page):
+class Veto(Page):
     form_model = 'player'
-    form_fields = ['veto1', 'veto2', 'rank1', 'rank2', 'rank3']
+    form_fields = ['choix1', 'choix2', 'choix3']
+    @staticmethod
+    def is_displayed(player: Player):
+        group = player.group
+        return player.id_in_group == 1
+
     @staticmethod
     def vars_for_template(player: Player):
         my_profile = player.preference_profile.split(",")
@@ -151,24 +116,63 @@ class Veto_Et_Classement(Page):
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
         # Récupérer les vétos depuis le formulaire HTML
-                player.vetos = f"{player.veto1},{player.veto2}"
+                player.choix = f"{player.choix1},{player.choix2},{player.choix3}"
 
-                # Récupérer et stocker le classement sous forme "c,d,e"
-                player.ranking = f"{player.rank1},{player.rank2},{player.rank3}"
+
     @staticmethod
     def error_message(player: Player, values):
 
         """ Vérifie qu'un même choix ne soit pas sélectionné plusieurs fois """
         selected_choices = [
-            values["veto1"], values["veto2"],
-            values["rank1"], values["rank2"], values["rank3"]
+            values["choix1"], values["choix2"], values["choix3"]
         ]
 
         if len(set(selected_choices)) != len(selected_choices):
             return "Vous ne pouvez pas sélectionner plusieurs fois le même choix (veto et classement)."
-class Attente_Avant_Tirage_Score(WaitPage):
-    after_all_players_arrive = calcul_meilleur_choix
+class J2_attente_veto_de_J1(WaitPage):
+    title_text = 'Attente des 3 choix du premier joueur'
+    @staticmethod
+    def is_displayed(player: Player):
+        group = player.group
+        return player.id_in_group == 2
+class Choix_de_J2(Page):
+    form_model = 'player'
+    form_fields = ['rank1']
+    @staticmethod
+    def is_displayed(player: Player):
+        group = player.group
+        return player.id_in_group == 2
+    @staticmethod
+    def vars_for_template(player: Player):
+        group = player.group
+        player1 = group.get_player_by_id(1)
+        player2 = group.get_player_by_id(2)
+        my_profile = player.preference_profile.split(",")
+        opponent_profile = player.opponent_profile.split(",")
+        all_choices = player2.preference_profile.split(",")
+        remaining_choices = player1.choix.split(",")
 
+
+        combined_profiles = [
+                    (rank + 1, my_choice, opp_choice)
+                    for rank, (my_choice, opp_choice) in enumerate(zip(my_profile, opponent_profile))
+                ]
+
+        return {    "remaining_choices": remaining_choices,
+                    'my_profile': player.preference_profile.split(","),
+                    'opponent_profile': player.opponent_profile.split(","),
+                    'combined_profiles': combined_profiles, # Liste prête pour le tableau
+                     "choix1": player1.choix1,
+                     "choix2": player1.choix2,
+                     "choix3": player1.choix3,
+                     'total_score': player.participant.vars["total_score"]
+                }
+class J1_attente_de_J2(WaitPage):
+    after_all_players_arrive = calcul_meilleur_choix
+    @staticmethod
+    def is_displayed(player: Player):
+        group = player.group
+        return player.id_in_group==1
 class CalculScoreChoix(Page):
     form_model = 'player'
     @staticmethod
@@ -187,7 +191,6 @@ class Attente_Avant_Resultat(WaitPage):
     def is_displayed(player: Player):
         return player.round_number == 4
 
-
 class pageFinale(Page):
     form_model = 'player'
     def is_displayed(player: Player):
@@ -204,5 +207,4 @@ class pageFinale(Page):
                 "classement": classement,
                 "scorePrem": meilleur_score}
 
-
-page_sequence = [Page_Accueil, Presentation_VR, Attribution_des_profils, Veto_Et_Classement, Attente_Avant_Tirage_Score, CalculScoreChoix, Attente_Avant_Resultat, pageFinale]
+page_sequence = [Page_Accueil, Presentation_VR, Attribution_des_profils, Veto, J2_attente_veto_de_J1, Choix_de_J2, J1_attente_de_J2, CalculScoreChoix, Attente_Avant_Resultat, pageFinale]
